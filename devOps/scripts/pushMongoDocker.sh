5ca5be0b4ae2e5f32ec16a7076ba33771875c427
ardiadrianadri <ardiadrianadri@gmail.com>
2015-11-02 21:20:09 +0100

    Añadimos los ficheros de base de datos

---
+++ devOps/scripts/pushMongoDocker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker push ardiadrianadri/mongo_node_one:1.0.7
+docker push ardiadrianadri/mongo_node_two:1.0.4
+docker push ardiadrianadri/mongo_node_three:1.0.4
+docker push ardiadrianadri/mongo_node_four:1.0.4
